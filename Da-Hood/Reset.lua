7b5a1a31a1e502ee245e5800b76e21b6af4791eb
Katusaii <91437317+Katusaii@users.noreply.github.com>
2021-09-26 22:58:57 +0100

    Update Reset.lua

--- Da-Hood/Reset.lua
+++ Da-Hood/Reset.lua
@@ -1,7 +1,6 @@
 local DeleteParts = {"LeftUpperArm", "LeftLowerArm", "LeftHand", "RightUpperArm", "RightLowerArm", "RightHand", "LeftUpperLeg", "LeftLowerLeg", "LeftFoot", "RightUpperLeg", "RightLowerLeg", "RightFoot", "UpperTorso", "LowerTorso", "Head", "Humanoid"};
 local Player = game:GetService("Players").LocalPlayer
 
-
 function Destroy()
     for _, v in pairs(Player.Character:GetChildren()) do
         if table.find(DeleteParts, v.Name) then
