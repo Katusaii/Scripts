78f060f617e9897b3830763e4bb229041489cb26
Katusaii <91437317+Katusaii@users.noreply.github.com>
2021-10-02 20:50:51 +0100

    Update Server-Crasher.lua

--- Da-Hood/Server-Crasher.lua
+++ Da-Hood/Server-Crasher.lua
@@ -11,6 +11,7 @@ getgenv().Crash = true
 getgenv().BuyMask = true
 
 local Amount = 0;
+local Percentage = 0;
 local DeleteParts = {"LeftUpperArm", "LeftLowerArm", "LeftHand", "RightUpperArm", "RightLowerArm", "RightHand", "LeftUpperLeg", "LeftLowerLeg", "LeftFoot", "RightUpperLeg", "RightLowerLeg", "RightFoot", "UpperTorso", "LowerTorso", "Head", "Humanoid"};
 local Player = game:GetService("Players").LocalPlayer;
 
@@ -33,15 +34,15 @@ end
 
 if BuyMask then
     Player.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Ignored.Shop["[Ninja Mask] - $60"].Head.CFrame
-    wait(0.3)
+    wait(0.5)
     fireclickdetector(game:GetService("Workspace").Ignored.Shop["[Ninja Mask] - $60"].ClickDetector)
-    wait(0.3)
+    wait(0.25)
     Player.Backpack.Mask.Parent = Player.Character
-    wait(0.3)
+    wait(0.25)
     Player.Character.Mask:Activate()
-    wait(0.3)
+    wait(0.25)
     Player.Character["In-gameMask"].Handle:Destroy()
-    wait(0.3)
+    wait(0.25)
     Player.Character.Mask.Parent = Player.Backpack
 end
 
@@ -50,16 +51,20 @@ game:GetService("RunService").Stepped:Connect(function()
         Player.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Ignored.Shop["[Starblox Latte] - $5"].Head.CFrame + Vector3.new(0, -5, 0)
         Player.Character.Humanoid:ChangeState("StrafingNoPhysics")
         wait()
-        fireclickdetector(game:GetService("Workspace").Ignored.Shop["[Starblox Latte] - $5"].ClickDetector)
+        if Amount ~= 400 and Percentage ~= 100 then
+            fireclickdetector(game:GetService("Workspace").Ignored.Shop["[Starblox Latte] - $5"].ClickDetector)
+        end
         wait()
         for _, v in pairs(Player.Backpack:GetChildren()) do
             if v.Name == "[Starblox Latte]" then
                 v.Parent = Player.Character
                 Amount = Amount + 1
-                print(Amount.." latte's")
+                Percentage = Percentage + 0.25
+                print("Amount: "..Amount.." | ".."Percentage: "..Percentage.."%")
             end
         end
-        if Amount == 400 then
+        if Amount == 400 and Percentage == 100 then
+            wait(1)
             Destroy()
         end
     end
