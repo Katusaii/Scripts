c8d502ff9d80b6c57644fcb34bb3ba597ec371b0
Katusaii <91437317+Katusaii@users.noreply.github.com>
2021-10-05 18:07:43 +0100

    Create Bypass.lua

---
+++ Da-Hood/Bypass.lua
@@ -0,0 +1,35 @@
+local Player = game:GetService("Players").LocalPlayer
+local RunService = game:GetService("RunService")
+
+local mt = getrawmetatable(game)
+local nc = mt.__namecall
+setreadonly(mt, false)
+
+mt.__namecall = newcclosure(function(self, ...)
+    local arguments = {...}
+    local method = getnamecallmethod() or get_namecall_method()
+    if method == "FireServer" and self.Name == "MainEvent" then
+        if arguments[1] == "CHECKER_1" or arguments[1] == "TeleportDetect" or arguments[1] == "OneMoreTime" then
+            return wait(9e9)
+        end
+    end
+    return nc(self, unpack(arguments))
+end)
+setreadonly(mt, true)
+
+function Destroy()
+    if Player.Character:FindFirstChild("") then
+        Player.Character:FindFirstChild(""):Destroy()
+    else
+        if Player.Backpack:FindFirstChild("") then
+            Player.Backpack:FindFirstChild(""):Destroy()
+        end
+    end
+end
+
+RunService.Heartbeat:Connect(function()
+    if Player.Character:FindFirstChildWhichIsA("Script"):FindFirstChildWhichIsA("LocalScript") then
+        Player.Character:FindFirstChildWhichIsA("Script"):FindFirstChildWhichIsA("LocalScript").Disabled = true
+    end
+    Destroy()
+end)
