2cbbd8063f7dbb962eb1cad19e84fad0a384e80f
Katusaii <91437317+Katusaii@users.noreply.github.com>
2021-10-14 19:21:55 +0100

    Update Reset.lua

--- Da-Hood/Reset.lua
+++ Da-Hood/Reset.lua
@@ -6,6 +6,14 @@ repeat wait() until game:GetService("Players").LocalPlayer.Character:FindFirstCh
 local DeleteParts = {"LeftUpperArm", "LeftLowerArm", "LeftHand", "RightUpperArm", "RightLowerArm", "RightHand", "LeftUpperLeg", "LeftLowerLeg", "LeftFoot", "RightUpperLeg", "RightLowerLeg", "RightFoot", "UpperTorso", "LowerTorso", "Head", "Humanoid"};
 local Player = game:GetService("Players").LocalPlayer
 
+function Unequip()
+    for _, v in pairs(Player.Character:GetChildren()) do
+        if v:IsA("Tool") then
+            v.Parent = Player.Backpack
+        end
+    end
+end
+
 function Destroy()
     for _, v in pairs(Player.Character:GetChildren()) do
         if table.find(DeleteParts, v.Name) then
@@ -14,4 +22,5 @@ function Destroy()
     end
 end
 
+Unequip()
 Destroy()
