4266fbb749e5e8cb3f7d455a8166e92bafbe807f
Katusaii <91437317+Katusaii@users.noreply.github.com>
2021-10-14 11:35:34 +0100

    Update Nuke.lua

--- Da-Hood/Nuke.lua
+++ Da-Hood/Nuke.lua
@@ -67,7 +67,7 @@ UserInput.InputBegan:Connect(function(Key, Typing)
     if Typing then return end
     if Key.KeyCode == Enum.KeyCode[ActivateNukeKey] then
         for _, v in pairs(Player.Character:GetChildren()) do
-            if v.Name == "[Grenade]" then
+            if v.Name == "[Grenade]" and Amount == 200 then
                 v:Activate()
             end
         end
